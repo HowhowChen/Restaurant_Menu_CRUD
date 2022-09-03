8a6cb22681d49e2dd4a64fb2231ac49b21f0c95a
Howhowchen <debby8698@gmail.com>
2022-09-03 20:57:36 +0800

    refactor: modify main.js from the javascript standard style

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,11 @@
 const ratingSort = document.querySelector('#rating-sort')
 const dataPanel = document.querySelector('#data-panel')
 const menus = document.querySelectorAll('#menu')
-const currentPage = document.querySelector('#current-page')   //current page
+const currentPage = document.querySelector('#current-page')// current page
 const pagination = document.querySelectorAll('.page-item')
 const menuObject = []
 
-/*sort function*/
+/* sort function */
 try {
   ratingSort.addEventListener('change', event => {
     const value = event.target.value
@@ -15,22 +15,17 @@ try {
       sortDownToUp()
     }
   })
-} catch {
-  
-}
+} catch {}
 
-/*show current page on pagination*/
+/* show current page on pagination */
 for (let i = 0; i < pagination.length; i++) {
   if (pagination[i].innerText === currentPage.textContent) {
     pagination[i].classList.add('active')
   }
 }
 
-
-
-
-//高到低
-function sortUpToDown() {
+/* 高 -> 低 */
+function sortUpToDown () {
   for (let i = 0; i < menus.length; i++) {
     menuObject.push({
       rating: menus[i].dataset.id,
@@ -38,7 +33,7 @@ function sortUpToDown() {
     })
   }
 
-  /*高 -> 低*/
+  /* 高 -> 低 */
   menuObject.sort((a, b) => {
     return b.rating - a.rating
   })
@@ -46,12 +41,10 @@ function sortUpToDown() {
   menuObject.forEach(item => {
     dataPanel.appendChild(item.html)
   })
-  
 }
-  
 
-//低到高
-function sortDownToUp() {
+/* 低 -> 高 */
+function sortDownToUp () {
   for (let i = 0; i < menus.length; i++) {
     menuObject.push({
       rating: menus[i].dataset.id,
@@ -59,7 +52,7 @@ function sortDownToUp() {
     })
   }
 
-  /*低 -> 高*/
+  /* 低 -> 高 */
   menuObject.sort((a, b) => {
     return a.rating - b.rating
   })
@@ -68,6 +61,3 @@ function sortDownToUp() {
     dataPanel.appendChild(item.html)
   })
 }
-
-
-
