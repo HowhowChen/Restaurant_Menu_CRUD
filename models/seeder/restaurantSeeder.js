56b4dc637c0f3bd4910f778a9e1f377fef7a781b
Howhowchen <debby8698@gmail.com>
2022-09-20 02:07:38 +0800

    modify todoSeeder

--- models/seeder/restaurantSeeder.js
+++ models/seeder/restaurantSeeder.js
@@ -1,13 +1,36 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config()
+}
 const Restaurant = require('../Restaurant')
 const restaurantList = require('../../restaurant.json')
-
 const db = require('../../config/mongoose')
+const User = require('../user')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example',
+  password: '12345678'
+}
 
 db.once('open', () => {
-  Restaurant.create(restaurantList.results)
-    .then(() => {
-      console.log('seeds saved')
-      db.close()//  結束database連線
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(async user => {
+      const userId = user._id
+      const restaurantArr = restaurantList.results
+      await Promise.all(restaurantArr.map(restaurant => {
+        restaurant.userId = userId
+        return Restaurant.create(restaurant)
+      }))
     })
-    .catch(error => console.log(error))
+    .then(() => {
+      console.log('done')
+      process.exit()
+    })
 })
