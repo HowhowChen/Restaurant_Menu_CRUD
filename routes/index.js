d255cfe6a7d5c4289a1d3a29b1766942614f9b4c
Howhowchen <debby8698@gmail.com>
2022-09-20 01:10:04 +0800

    feat: add facebook login

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
+
 const { authenticator } = require('../middleware/auth')
 
 router.use('/restaurants', authenticator, restaurants)  // 加入驗證程序
 router.use('/users', users)
+router.use('/auth', auth) // Oauth
 router.use('/', authenticator, home)  // 加入驗證程序
 
 module.exports = router
