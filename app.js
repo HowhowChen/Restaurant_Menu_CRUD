4be3f49251c73d03f7bead14dd681ebb69c7ecca
Howhowchen <debby8698@gmail.com>
2022-09-19 20:42:20 +0800

    feat: add user modules

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const routes = require('./routes')
 require('./config/mongoose')
 
