87b056cd344314e388bdd1cc62a7102ac53f9d81
Howhowchen <debby8698@gmail.com>
2022-09-19 21:12:42 +0800

    feat: add user registration feature

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 //  setting mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection
 
 db.on('error', () => console.log('MongoDB Error'))
