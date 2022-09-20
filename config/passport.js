3f1310d78fcf5cc11cd39cd1f4b6a13535c2d5dd
Howhowchen <debby8698@gmail.com>
2022-09-21 02:31:48 +0800

    feat: add google login

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const User = require('../models/user')
 const localStrategy = require('passport-local').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth2').Strategy
 const bcrypt = require('bcryptjs')
 
 module.exports = app => {
@@ -26,30 +27,24 @@ module.exports = app => {
       .catch(error => done(error, false))
   }))
 
-  //  設定facebook Oauth策略
+  //  設定facebook OAuth策略
   passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_ID,
     clientSecret: process.env.FACEBOOK_SECRET,
     callbackURL: process.env.FACEBOOK_CALLBACK,
     profileFields: ['email', 'displayName']
-  }, (accessToken, refreshToken, profile, done) => {
-    const { name, email } = profile._json
-    User.findOne({ email })
-      .then(user => {
-        if (user) return done(null, user)
-        const randomPassword = Math.random().toString(36).slice(-8)
-        bcrypt
-          .genSalt(10)
-          .then(salt => bcrypt.hash(randomPassword, salt))
-          .then(hash => User.create({
-            name,
-            email,
-            password: hash
-          }))
-          .then(user => done(null, user))
-          .catch(err => done(err, false))
-      })
-  }))
+  }, 
+    thirdPartyOAuthCallback
+  ))
+
+  //  設定google OAuth策略
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK,
+  },
+    thirdPartyOAuthCallback 
+  ))
 
   //  設定序列化與反序列化
   passport.serializeUser((user, done) => {
@@ -61,4 +56,23 @@ module.exports = app => {
       .then(user => done(null, user))
       .catch(err => done(err, null))
   })
+}
+
+function thirdPartyOAuthCallback(accessToken, refreshToken, profile, done) {
+  const { name, email } = profile._json
+  User.findOne({ email })
+    .then(user  => {
+      if (user) return done(null, user)
+      const randomPassword = Math.random().toString(36).slice(-8)
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(randomPassword, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
+        .then(user => done(null, user))
+        .catch(err => done(err, false))
+    })
 }
